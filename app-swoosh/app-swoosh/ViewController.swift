c77745be1dc372bed14a473f80ebf0e0da861efe
user <user@users-MacBook-Pro.local>
2018-09-26 15:30:15 -0400

    minor changes

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by user on 9/24/18.
+//  Copyright © 2018 Bradley Carter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImage: UIImageView!
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    
+}
+
